1ced645a0c33f9e2c191055d37b789e66d700f8b
sofia study <sofiastudy200517@gmail.com>
2025-07-02 11:20:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import streamlit as st
 from ui.login import login_ui
 from ui.analyzer import analyzer_ui
 from ui.history import history_ui
+from ui.style import style_ui
 
 st.set_page_config(page_title="Mental Health Analyzer", layout="centered")
 
 if login_ui():
     analyzer_ui()
     history_ui()
+    style_ui()
