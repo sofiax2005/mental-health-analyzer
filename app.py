c1786032a7d58db806022db48d64c156c5f521f6
sofia study <sofiastudy200517@gmail.com>
2025-07-02 12:01:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 # app.py
 import streamlit as st
+from ui.styles import get_mood_gradient
 from ui.login import login_ui
 from ui.analyzer import analyzer_ui
 from ui.history import history_ui
-from ui.styles import apply_theme
-
 st.set_page_config(page_title="Mental Health Analyzer", layout="centered")
 
 if login_ui():
